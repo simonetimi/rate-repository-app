4bc1bfc291de3cfa143ab82738b1396e17210caa
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-28 23:06:18 +0100

    Complete exercise 10.6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,6 @@
-import { View, StyleSheet, Pressable } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Link } from 'react-router-native';
 import Text from './Text';
 import Constants from 'expo-constants';
 
@@ -9,20 +11,35 @@ const styles = StyleSheet.create({
     paddingRight: 12,
     paddingBottom: 10,
     backgroundColor: 'rgba(0, 100, 200, 0.5)',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   title: {
     fontSize: 24,
+    color: '#333',
+  },
+  login: {
+    marginLeft: 'auto',
+  },
+  link: {
+    activeOpacity: 0,
   },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Pressable>
+      <Link to="/" activeOpacity={0} underlayColor={'transparent'} style={styles.link}>
         <Text fontWeight={'bold'} style={styles.title}>
           Repositories
         </Text>
-      </Pressable>
+      </Link>
+      <Link to="/signin" activeOpacity={0} underlayColor={'transparent'} style={styles.link}>
+        <Text fontWeight={'bold'} style={[styles.title, styles.login]}>
+          Login
+        </Text>
+      </Link>
     </View>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
 import { StyleSheet, View } from 'react-native';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
+import SignIn from './SignIn';
+import { Route, Routes, Navigate } from 'react-router-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,7 +17,11 @@ const Main = () => {
     <>
       <AppBar />
       <View style={styles.container}>
-        <RepositoryList />
+        <Routes>
+          <Route path="/" element={<RepositoryList />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </View>
     </>
   );
