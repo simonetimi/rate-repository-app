fca1ced322d4e9c8562f87643cd711e2aad8b57d
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-28 23:25:54 +0100

    Complete exercise 10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import { Link } from 'react-router-native';
 import Text from './Text';
 import Constants from 'expo-constants';
@@ -7,20 +7,20 @@ import Constants from 'expo-constants';
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    paddingLeft: 12,
-    paddingRight: 12,
     paddingBottom: 10,
+    paddingLeft: 5,
+    paddingRight: 5,
+    gap: 14,
     backgroundColor: 'rgba(0, 100, 200, 0.5)',
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'flex-start',
     alignItems: 'center',
   },
   title: {
     fontSize: 24,
     color: '#333',
-  },
-  login: {
-    marginLeft: 'auto',
+    marginRight: 5,
+    marginLeft: 5,
   },
   link: {
     activeOpacity: 0,
@@ -30,16 +30,22 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Link to="/" activeOpacity={0} underlayColor={'transparent'} style={styles.link}>
-        <Text fontWeight={'bold'} style={styles.title}>
-          Repositories
-        </Text>
-      </Link>
-      <Link to="/signin" activeOpacity={0} underlayColor={'transparent'} style={styles.link}>
-        <Text fontWeight={'bold'} style={[styles.title, styles.login]}>
-          Login
-        </Text>
-      </Link>
+      <ScrollView
+        horizontal
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      >
+        <Link to="/" activeOpacity={0} underlayColor={'transparent'} style={styles.link}>
+          <Text fontWeight={'bold'} style={styles.title}>
+            Repositories
+          </Text>
+        </Link>
+        <Link to="/signin" activeOpacity={0} underlayColor={'transparent'} style={styles.link}>
+          <Text fontWeight={'bold'} style={styles.title}>
+            Login
+          </Text>
+        </Link>
+      </ScrollView>
     </View>
   );
 };
